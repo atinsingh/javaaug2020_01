c1b8363a9550f7bf2468e48fa92788f6f752de53
Singh,Atin <atin@pragra.co>
2020-09-08 21:57:17 -0400

    added solution to madMax5

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
         // write a logic to print number which is bigger
         int bigNumber = a>b ? a:b; // ternary
         System.out.println(bigNumber);
+
+
+    }
+
+    public int maxMod5(int a, int b) {
+        if(a==b) return 0;
+        if(  a%5 == b%5 ) {
+            return a < b ? a: b;
+        }
+        return a > b ? a: b;
     }
 }
